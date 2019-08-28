668ad41987494affb0cb1f74d1f340f49f769679
malonge <malonge11@gmail.com>
2019-08-28 16:28:07 -0400

    updated checking script to take in a vcf file

--- dc_check.py
+++ dc_check.py
@@ -3,6 +3,14 @@ import sys
 import argparse
 
 import numpy as np
+from scipy import stats
+
+
+def fit_gaussian(cov_dict, trim=0):
+    """ Fit a gaussian distribution to the coverage across bins genome-wide. """
+    # Later will trim off outliers
+    return stats.norm.fit(np.asarray(list(cov_dict.values())).flatten())
+
 
 def get_adj_dup_len(dup_len, bin_size):
     new_dup_len = (dup_len//bin_size)*bin_size
@@ -22,54 +30,89 @@ def get_lop_size(dup_start, adj_dup_len, bin_size):
 
 
 def main():
-    #parser = argparse.ArgumentParser(description='Check DUPs in a VCF file for increased short-read coverage')
-    #parser.add_argument("vcf", metavar="<vars.vcf>", type=str, help="VCF file containing DUPs to check. May contain non-dups.")
+    parser = argparse.ArgumentParser(description='Check DUPs in a VCF file for increased short-read coverage')
+    parser.add_argument("vcf", metavar="<vars.vcf>", type=str, help="VCF file containing DUPs to check. May contain non-dups.")
+    parser.add_argument("cov", metavar="<coverage.bed>", type=str, help="Bed file from dc_index-bedtools-dc_gccorr")
 
-    #args = parser.parse_args()
-    #vcf_file = args.vcf
-
-    dup_start = int(sys.argv[2])
-    dup_end = int(sys.argv[3])
-    cov_file = sys.argv[1] # For now, assumes its just for a single chromosomes
-
-    dup_len = dup_end - dup_start
+    # Get command line args
+    args = parser.parse_args()
+    vcf_file = args.vcf
+    cov_file = args.cov
     bin_size = 200
 
-    # Read in the array of bin coverages
-    covs = []
-    with open(cov_file, 'r') as f:
+    # Read the coverage file
+    chr_covs = dict()
+    with open(cov_file, "r") as f:
         for line in f:
-            L1 = line.rstrip().split("\t")
-            covs.append(int(round(float(L1[8]))))
-    cov_arr = np.asarray(covs, dtype=np.int32)
+            header, start, end, gc, a, b, c, d, corr_cov = line.rstrip().split("\t")
+            if header not in chr_covs:
+                chr_covs[header] = []
+            chr_covs[header].append(int(round(float(corr_cov))))
+
+    for i in chr_covs:
+        chr_covs[i] = np.asarray(chr_covs[i], dtype=np.int32)
+
+    # Get the average coverage across the initial bins
+    glob_mean, glob_std = fit_gaussian(chr_covs)
+
+    # Process each line of the vcf file. If its a comment or non dup, just print it.
+    with open(vcf_file, "r") as f:
+        for line in f:
+            line = line.rstrip()
+            if line.startswith("#"):
+                print(line)
+            else:
+                fields = line.split("\t")
+                sv_type = ''
+                sv_len = 0
+                for tag in fields[8]:
+                    if tag.startswith("SVTYPE="):
+                        sv_type = tag[7:]
+                    if tag.startswith("SVLEN="):
+                        sv_len = int(tag[6:])
+
+                assert sv_type
+                assert sv_len
+                if sv_type != "DUP":
+                    print(line)
+                else:
+                    # This is a duplication, so lets try to verify it.
+                    dup_header = fields[0]
+                    dup_start = int(fields[1])
+                    dup_end = dup_start + sv_len
+                    dup_len = sv_len
+                    cov_arr = chr_covs[dup_header]
     
-    # Get the adjusted dup length which is as close to the real dup length as possible
-    # but divisible by the bin size.
-    adj_dup_len = get_adj_dup_len(dup_len, bin_size)
-    print(dup_len, adj_dup_len)
+                    # Get the adjusted dup length which is as close to the real dup length as possible
+                    # but divisible by the bin size.
+                    adj_dup_len = get_adj_dup_len(dup_len, bin_size)
+                    print(dup_len, adj_dup_len)
 
-    # Decide how many windows we should shave off the start to get close to the
-    # real starting point.
-    lop_size = get_lop_size(dup_start, adj_dup_len, bin_size)
-    print("lop_size = %r" %lop_size)
+                    # Decide how many windows we should shave off the start to get close to the
+                    # real starting point.
+                    lop_size = get_lop_size(dup_start, adj_dup_len, bin_size)
+                    print("lop_size = %r" % lop_size)
 
-    # Lop off the required number of bins
-    lop_cov_arr = cov_arr[lop_size:]
+                    # Lop off the required number of bins
+                    lop_cov_arr = cov_arr[lop_size:]
 
-    # Merge bins into the necessary adjusted dup size
-    factor = adj_dup_len//bin_size
-    print("loped coverage array has length= %r" %(len(lop_cov_arr)))
-    
-    # Truncate the coverage array so that it is divisible by the factor
-    trunc_cov_arr = lop_cov_arr[:(len(lop_cov_arr)//factor)*factor]
-    cov_mat = trunc_cov_arr.reshape(len(trunc_cov_arr)//factor, factor)
-    merged_cov_arr = np.average(cov_mat, axis=1)
-    print(len(trunc_cov_arr), len(merged_cov_arr))
+                    # Merge bins into the necessary adjusted dup size
+                    factor = adj_dup_len//bin_size
+                    print("loped coverage array has length= %r" %(len(lop_cov_arr)))
 
-    # Now our coverage is merged and aligned. Let's see the coverage of our duplication window
-    print("DUP bin coverage = %f" %(merged_cov_arr[dup_start//adj_dup_len]))
+                    # Truncate the coverage array so that it is divisible by the factor
+                    trunc_cov_arr = lop_cov_arr[:(len(lop_cov_arr)//factor)*factor]
+                    cov_mat = trunc_cov_arr.reshape(len(trunc_cov_arr)//factor, factor)
+                    merged_cov_arr = np.average(cov_mat, axis=1)
+                    print(len(trunc_cov_arr), len(merged_cov_arr))
 
-    # And the coverage of all of the bins that size
-    print("average coverage of bins of the same size = %f" %(np.average(merged_cov_arr)))
+                    # Now our coverage is merged and aligned. Let's see the coverage of our duplication window
+                    print("DUP bin coverage = %f" %(merged_cov_arr[dup_start//adj_dup_len]))
+
+                    # And the coverage of all of the bins that size
+                    print("average coverage of original bins = %f" % glob_mean)
+                    print("\n\nmoving on, and getting over\n\n")
+
+
 if __name__ == "__main__":
     main()
